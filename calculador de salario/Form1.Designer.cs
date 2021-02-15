e98b11e7aa70dbf92edcbd9e80440326438c85ed
Javier Enrique Hernandez Marquez <77635240+JavierHernandez7-bit@users.noreply.github.com>
2021-02-14 23:08:43 -0600

    Ejercicio Guia 5

---
+++ calculador de salario/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace calculador_de_salario
+{
+    partial class posLab
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Titulo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.combBoxPuesto = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fecha = new System.Windows.Forms.Label();
+            this.hora = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Titulo
+            // 
+            this.Titulo.AutoSize = true;
+            this.Titulo.Font = new System.Drawing.Font("Verdana", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo.Location = new System.Drawing.Point(58, 35);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(644, 42);
+            this.Titulo.TabIndex = 0;
+            this.Titulo.Text = "Introduzca los datos requeridos";
+            this.Titulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre del empleado:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(188, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Salario:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(114, 128);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 13);
+            this.linkLabel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(392, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textNombre
+            // 
+            this.textNombre.BackColor = System.Drawing.SystemColors.Window;
+            this.textNombre.Location = new System.Drawing.Point(259, 107);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(221, 20);
+            this.textNombre.TabIndex = 5;
+            this.textNombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(259, 160);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(127, 20);
+            this.txtSalario.TabIndex = 6;
+            // 
+            // combBoxPuesto
+            // 
+            this.combBoxPuesto.FormattingEnabled = true;
+            this.combBoxPuesto.Location = new System.Drawing.Point(259, 207);
+            this.combBoxPuesto.Name = "combBoxPuesto";
+            this.combBoxPuesto.Size = new System.Drawing.Size(127, 21);
+            this.combBoxPuesto.TabIndex = 7;
+            this.combBoxPuesto.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(114, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Posición Laboral:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(638, 272);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 27);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Salir del programa";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // fecha
+            // 
+            this.fecha.AutoSize = true;
+            this.fecha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.fecha.Location = new System.Drawing.Point(601, 9);
+            this.fecha.Name = "fecha";
+            this.fecha.Size = new System.Drawing.Size(35, 13);
+            this.fecha.TabIndex = 10;
+            this.fecha.Text = "label4";
+            // 
+            // hora
+            // 
+            this.hora.AutoSize = true;
+            this.hora.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.hora.Location = new System.Drawing.Point(12, 272);
+            this.hora.Name = "hora";
+            this.hora.Size = new System.Drawing.Size(35, 13);
+            this.hora.TabIndex = 11;
+            this.hora.Text = "label4";
+            // 
+            // posLab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(770, 301);
+            this.Controls.Add(this.hora);
+            this.Controls.Add(this.fecha);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.combBoxPuesto);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Titulo);
+            this.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "posLab";
+            this.RightToLeftLayout = true;
+            this.Text = "Calculadora de Salario";
+            this.Load += new System.EventHandler(this.posLab_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.ComboBox combBoxPuesto;
+        private System.Windows.Forms.Label label3;
+        internal System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label fecha;
+        private System.Windows.Forms.Label hora;
+    }
+}
+
---
+++ calculador de salario/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculador_de_salario
+{
+    public partial class posLab : Form
+    {
+        public posLab()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre = textNombre.Text;
+         //Text Box
+            string puesto = combBoxPuesto.Text;
+            double salarionormal = double.Parse(txtSalario.Text);
+            double descuento = 0.20;
+            double salariofinal = 0;
+
+            switch (puesto)
+            {
+                case "Gerente":
+                    descuento = salarionormal * 0.20;
+                    salariofinal = salarionormal - descuento;
+                    break;
+
+                case "SubGerente":
+                    descuento = salarionormal * 0.15;
+                    salariofinal = salarionormal - descuento;
+                    break;
+
+                case "Secretaria":
+                    descuento = salarionormal * 0.05;
+                    salariofinal = salarionormal - descuento;
+                    break;
+            }
+
+            //MessageBox.Show("Bienvenido a POO " + nombre + " este es tu primer formulario");
+            MessageBox.Show(" Estimado " + nombre + ", su salario aplicando descuentos será de: " + salariofinal + " Dolares Norte Americanos");
+
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void posLab_Load(object sender, EventArgs e)
+        {
+            combBoxPuesto.Items.Clear();
+            combBoxPuesto.Items.Add("Gerente");
+            combBoxPuesto.Items.Add("Subgerente");
+            combBoxPuesto.Items.Add("Secretaria");
+            fecha.Text = DateTime.Now.ToLongDateString();
+            hora.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
